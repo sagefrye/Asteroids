ca92809a33921124a173bc3c4789c8a7771299ea
Sage Frye <sagefrye@Sages-Air.localdomain>
2025-08-10 10:24:36 -0500

    added player

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,15 +1,23 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    fps = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
+        delta = fps.tick(60)
+        dt = delta / 1000
+
 if __name__ == "__main__":
     main()
